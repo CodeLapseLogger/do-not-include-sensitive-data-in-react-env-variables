594cadc9b483c4b9adfe412a4a861596e2448f07
Surya T N <suryatej.nim@yahoo.com>
2023-04-21 23:35:22 +0530

    feat: add middleware to express backend for handling news data get request
    Implemented middleware in Express.js backend to handle GET
    request mapped to url: /news-data, to in turn make the
    required API call to News API end-point to fetch the desired
    news data, to be passed backed to client as a JSON response.
    So, it just sends the data back to the React front-end client
    and not the typical HTML.
    
    The Express.js back-end server has been configured to listen in
    on port 3001 for client requests, as the port 3000 is already
    taken by the front-end React's web server.
    
    Additional npm packages have been installed (dotenv, cors, axios)
    in the node-express backend to ensure the news api is picked up
    by the Express server from the environment variables (dotenv),
    to make the api call to third-party api end-point (axios) and
    support cross-origin requests (cors) where React client on port
    3000 makes a request to Express backend on port 3001, a different
    port or origin.
    
    BREAKING CHANGE: Express back-end server handles the delegated task
    of sending a request to third party News API end-point, processing
    the response and in turn catapults the fetched news data as a JSON
    response to the React client

--- express-backend/server.js
+++ express-backend/server.js
@@ -1,11 +1,65 @@
+const axios = require("axios").default;
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const allowedOriginList = ["http://localhost:3000"];
+const newsAPIUrl =
+  "https://newsapi.org/v2/top-headlines?country=in&category=general";
+const newsAPIKey = process.env.NEWS_API_KEY;
 
-app.get("/", (req, res) => {
-  res.send(
-    "This is the response served through Express server for the GET request to server root: '/'"
+const responseStatusToErrorMessageMapping = {
+  401: "NewsAPI key has expired or is invalid",
+};
+
+app.use(cors());
+
+app.get("/news-data", (req, res) => {
+  const requestOrigin = req.get("origin");
+
+  const matchingOrigin = allowedOriginList.find((allowedOrigin) =>
+    allowedOrigin.toLowerCase().includes(requestOrigin)
   );
+
+  if (matchingOrigin === undefined) {
+    res.status(403).send({ error: "Access Forbidden" });
+    console.log("Didn't match origin");
+  }
+
+  let newsAPIData = null;
+  axios
+    .get(newsAPIUrl, {
+      headers: {
+        "X-Api-Key": newsAPIKey,
+      },
+    })
+    // .then((response) => response.json())
+    .then((responseJSONData) => {
+      for (const [key, value] of Object.entries(
+        responseJSONData.data.articles
+      )) {
+        console.log(`${key}, ${Object.entries(value)}`);
+      }
+      newsAPIData = { articles: responseJSONData.data.articles };
+      // res.append("Access-Control-Allow-Origin", "*");
+      // res.append("Content-Type", "application/json");
+      res.status(200).json(newsAPIData);
+    })
+    .catch((error) => {
+      const errorResponseStatusCode = error.response.status;
+      console.log(`Error response status: ${errorResponseStatusCode}`);
+      let errorMessage = null;
+      if (errorResponseStatusCode === 401) {
+        errorMessage =
+          responseStatusToErrorMessageMapping[errorResponseStatusCode];
+      } else {
+        errorMessage = error.message;
+      }
+
+      res.status(500).json({ message: errorMessage });
+    });
 });
 
 app.listen(port, () => {
